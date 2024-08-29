a62aef1663d27f2d94f80f13845dd45c01a60266
muer-zhou <150397700+muer-zhou@users.noreply.github.com>
2024-08-28 21:25:34 -0700

    added source file for assignment

---
+++ cis25assignment1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 1;
+    int num2 = 2; 
+    
+    num1 = 2;
+    num2 = 1;
+
+    cout << "hello" << num1 << " & " << num2 << endl;
+
+    return 0;
+}
