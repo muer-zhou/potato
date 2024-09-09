5a0c349c8316309e1e1eaa0ac155e99e94768a79
cybermoon <muer.zhou@berkeley.edu>
2024-09-08 21:08:16 -0700

    Fix swapping logic in cis25assignment1.cpp

--- cis25assignment1.cpp
+++ cis25assignment1.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-using namespace std;
+using namespace std;int main()
+{
+	int num1 = 1;
+	int num2 = 2;
+	int temp;
 
-int main() {
-    int num1 = 1;
-    int num2 = 2; 
+	temp = num1;
+	num1 = num2;
+	num2 = temp;
     
-    num1 = 2;
-    num2 = 1;
-
-    cout << "hello" << num1 << " & " << num2 << endl;
-
+	cout << num1 << ", " << num2 << endl;      
     return 0;
 }
