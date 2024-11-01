b06560a40509ab2b564df884aeab07cc37c7f6f4
cybermoon <muer.zhou@berkeley.edu>
2024-10-31 21:11:07 -0700

    Assignment 20

--- classassignment20/assignment20.cpp
+++ classassignment20/assignment20.cpp
@@ -1,55 +1,24 @@
+// ArrayUtils.cpp
 #include <iostream>
-using namespace std;
+#include "ArrayUtils.h"
 
-int* expand(int *oldArr, int oldSize, int newSize) { // The int* returns a pointer & expand is a function
-    // int *arr means pointer to an array
-    int *newArr = new int[newSize]; // This is how you declare a new array in the heap 
-    for(int i = 0; i < oldSize; i++) { // it iterates through the old array
-        newArr[i] = oldArr[i]; //sets the value of each element in the old array to the new array 
-    }
-    for (int i = oldSize; i < newSize; i++) { // initalize the rest of the values to zero
-        newArr[i] = 0;
-    }
-    delete [] oldArr; // deallocates old array
-    oldArr = nullptr; // prevent hanging pointers 
-    return newArr; // returns new array 
-} // entire function expands new array 
-// function to expand our dynamically allocated array 
-// a nornal array stored in the stack must be known at compile time (this is the reason why we are using dynamically allocated arrays )
-
-// a pointer is a memory address
-
-void printArray(int *arr, int size) {
-    for(int i = 0; i < size; i++) {
-        cout << arr[i] << endl;
-    }
-}
-
-int main() {
-    int *arr = new int[1]; // creating a new interger array with size one in the heap dynamically allocated 
-    int size = 1;
-    for (int i = 0; true; i++) { // run indefinitely
-        cout << "Enter a number, or type 's' to stop: ";
-        string userInput = ""; // makes it a string 
-        cin >> userInput;
-        if (userInput == "s") {
-            break;
+namespace ArrayOps {
+    int* expand(int *oldArr, int oldSize, int newSize) {
+        int *newArr = new int[newSize];
+        for (int i = 0; i < oldSize; i++) {
+            newArr[i] = oldArr[i];
         }
-        int num = stoi(userInput); 
-        if (i >= size) { // to check if the array is at max capacity
-            arr = expand(arr, size, 2*size); // it doubles the size of the array
-            size *= 2;
-            arr[i] = num;
+        for (int i = oldSize; i < newSize; i++) {
+            newArr[i] = 0;
         }
-        else {
-            arr[i] = num;
-        }
+        delete[] oldArr;
+        oldArr = nullptr;
+        return newArr;
+    }
 
+    void printArray(int *arr, int size) {
+        for (int i = 0; i < size; i++) {
+            std::cout << arr[i] << std::endl;
+        }
     }
-    int sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += arr[i];
-    }
-    printArray(arr, size);
-    return 0;
 }
---
+++ classassignment20/main.cpp
@@ -0,0 +1,31 @@
+// main.cpp
+#include <iostream>
+#include <string>
+#include "ArrayUtils.h"
+
+using namespace std;
+using namespace ArrayOps;
+
+int main() {
+    int *arr = new int[1];
+    int size = 1;
+    for (int i = 0; true; i++) {
+        cout << "Enter a number, or type 's' to stop: ";
+        string userInput;
+        cin >> userInput;
+        if (userInput == "s") {
+            break;
+        }
+        int num = stoi(userInput);
+        if (i >= size) {
+            arr = expand(arr, size, 2 * size);
+            size *= 2;
+            arr[i] = num;
+        } else {
+            arr[i] = num;
+        }
+    }
+    printArray(arr, size);
+    delete[] arr;  // Deallocate final array to prevent memory leaks
+    return 0;
+}
